2e0799aae964eb244aa25258064050c90d9581d5
VinaySarilla <vinayknown@gmail.com>
2022-04-22 12:23:08 +0530

    backend url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = "http://localhost:8080"
+const URL = "https://2037-103-251-50-170.in.ngrok.io"
 
 const getProducts = async () => {
     return axios.get(`${URL}/getProducts`).then(res => res.data)
